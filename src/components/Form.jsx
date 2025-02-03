e6e7532e2f51eb2471b9cc7e9c674e8dbb31df3f
ashish <guptashish101020@gmail.com>
2025-02-03 16:35:44 +0530

    update form file

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const giveAlert = () => {
+    alert(name + " " + email + " " + password);
+
+    setName('')
+    setEmail('')
+    setPassword('')
+  };
+
   return (
     <form action="">
-        <div>
-            Name: <input type="text" />
-        </div>
-        <div>
-            Email: <input type="email" />
-        </div>
-        <div>
-            Password: <input type="password" />
-        </div>
+      <div>
+        Name:{" "}
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+        />
+      </div>
+      <div>
+        Email:{" "}
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+        />
+      </div>
+      <div>
+        Password:{" "}
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+        />
+      </div>
+      <button onClick={giveAlert}>Submit</button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
